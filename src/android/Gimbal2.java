ab1baccf310da10892cba01af07a3df806cf07c3
Andrea Montemaggio <a.montemaggio@beyond-design.it>
2016-07-23 10:48:48 +0200

    Added CommunicationListener with handlers for notifications. Added registerForPush wrapper.

--- src/android/Gimbal2.java
+++ src/android/Gimbal2.java
@@ -5,11 +5,13 @@ package io.hpd.cordova.gimbal2;
  * Denny Tsai <happydenn@happydenn.net>
  */
 
+import java.util.Collection;
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
 import android.util.Log;
 
+import com.gimbal.android.*;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
@@ -17,12 +19,6 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import com.gimbal.android.Gimbal;
-import com.gimbal.android.BeaconManager;
-import com.gimbal.android.BeaconEventListener;
-import com.gimbal.android.BeaconSighting;
-import com.gimbal.android.Beacon;
 
 
 public class Gimbal2 extends CordovaPlugin {
@@ -33,7 +29,9 @@ public class Gimbal2 extends CordovaPlugin {
     private BeaconManager beaconManager;
     private BeaconEventListener beaconEventListener;
 
-	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    private CommunicationListener communicationListener;
+
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("initialize")) {
             if (args.length() != 1) return false;
 
@@ -80,6 +78,106 @@ public class Gimbal2 extends CordovaPlugin {
             beaconManager = new BeaconManager();
             beaconManager.addListener(beaconEventListener);
 
+            communicationListener = new CommunicationListener(){
+                @Override
+                public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Push push) {
+                    Log.i("INFO", "presentNotificationForCommunications (PUSH): " + push.toString());
+
+                    for (Communication communication : communications) {
+                        JSONObject responseObject = new JSONObject();
+
+                        try {
+                            responseObject.put("event", "onCommunicationPresentedPush");
+                            responseObject.put("pushType", push.getPushType().toString());
+                            responseObject.put("identifier", communication.getIdentifier());
+                            responseObject.put("title", communication.getTitle());
+                            responseObject.put("description", communication.getDescription());
+                            responseObject.put("deliveryDate", simpleDateFormat.format(new Date(communication.getDeliveryDate())));
+                            responseObject.put("expiryTimeInMillis", communication.getExpiryTimeInMillis());
+                            responseObject.put("url", communication.getURL());
+                            responseObject.put("attributes", parseAttributes(communication.getAttributes()));
+                        } catch (JSONException e) {
+                            Log.e("Error", e.getMessage(), e);
+                        }
+
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, responseObject);
+                        pluginResult.setKeepCallback(true);
+
+                        gimbalCallbackContext.sendPluginResult(pluginResult);
+                    }
+
+                    return communications;
+                }
+
+                @Override
+                public Collection<Communication> presentNotificationForCommunications(Collection<Communication> communications, Visit visit) {
+                    Log.i("INFO", "presentNotificationForCommunications (VISIT): " + visit.toString());
+
+                    for (Communication communication : communications) {
+                        JSONObject responseObject = new JSONObject();
+
+                        try {
+                            responseObject.put("event", "onCommunicationPresentedVisit");
+                            responseObject.put("identifier", communication.getIdentifier());
+                            responseObject.put("title", communication.getTitle());
+                            responseObject.put("description", communication.getDescription());
+                            responseObject.put("deliveryDate", simpleDateFormat.format(new Date(communication.getDeliveryDate())));
+                            responseObject.put("expiryTimeInMillis", communication.getExpiryTimeInMillis());
+                            responseObject.put("url", communication.getURL());
+                            responseObject.put("visit", parseVisit(visit));
+                            responseObject.put("attributes", parseAttributes(communication.getAttributes()));
+                        } catch (JSONException e) {
+                            Log.e("Error", e.getMessage(), e);
+                        }
+
+                        PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, responseObject);
+                        pluginResult.setKeepCallback(true);
+
+                        gimbalCallbackContext.sendPluginResult(pluginResult);
+                    }
+
+                    return communications;
+                }
+
+                private JSONObject parseVisit(Visit visit) throws JSONException {
+                    JSONObject oVisit = new JSONObject();
+
+                    oVisit.put("arrivalTime", simpleDateFormat.format(new Date(visit.getArrivalTimeInMillis())));
+                    oVisit.put("departureTime", simpleDateFormat.format(new Date(visit.getDepartureTimeInMillis())));
+                    oVisit.put("dwellTime", simpleDateFormat.format(new Date(visit.getDwellTimeInMillis())));
+                    oVisit.put("visitId", visit.getVisitID());
+                    oVisit.put("place", parsePlace(visit.getPlace()));
+
+                    return oVisit;
+                }
+
+                private JSONObject parsePlace(Place place) throws JSONException {
+                    JSONObject oPlace = null;
+                    if(place != null){
+                        oPlace = new JSONObject();
+                        oPlace.put("identifier", place.getIdentifier());
+                        oPlace.put("name", place.getName());
+                        oPlace.put("attributes", parseAttributes(place.getAttributes()));
+                    }
+
+                    return oPlace;
+                }
+
+                private JSONObject parseAttributes(Attributes attrs) throws JSONException {
+                    JSONObject oAttributes = null;
+                    if(attrs != null) {
+                        oAttributes = new JSONObject();
+                        for (String k : attrs.getAllKeys()) {
+                            oAttributes.put(k, attrs.getValue(k));
+                        }
+                    }
+
+                    return oAttributes;
+                }
+            };
+
+            CommunicationManager.getInstance().addListener(communicationListener);
+
             return true;
         }
 
@@ -97,7 +195,15 @@ public class Gimbal2 extends CordovaPlugin {
             return true;
         }
 
-		return false;
-	}
+        if (action.equals("registerForPush")) {
+            if (args.length() != 1)
+                return false;
 
+            String gcm = args.getString(0);
+            Gimbal.registerForPush(gcm);
+            return true;
+        }
+
+        return false;
+    }
 }
--- www/Gimbal2.js
+++ www/Gimbal2.js
@@ -21,6 +21,12 @@ Gimbal2.prototype.eventCallback = function(data) {
     if (data.event == 'onBeaconSighting') {
         cordova.fireWindowEvent('beaconsighting', data);
     }
+    else if(data.event == 'onCommunicationPresentedPush'){
+        cordova.fireWindowEvent('gimbal_communicationPresentedPush', data);
+    }
+    else if(data.event == 'onCommunicationPresentedVisit'){
+        cordova.fireWindowEvent('gimbal_communicationPresentedVisit', data);
+    }
 };
 
 Gimbal2.prototype.errorCallback = function(data) {
@@ -35,6 +41,10 @@ Gimbal2.prototype.startBeaconManager = function() {
 Gimbal2.prototype.stopBeaconManager = function() {
     if (!this.hasInitialized) return;
     exec(null, null, "Gimbal2", "stopBeaconManager", []);
+};
+
+Gimbal2.prototype.registerForPush = function(gcm) {
+    exec(this.eventCallback, this.errorCallback, "Gimbal2", "registerForPush", [gcm]);
 };
 
 var gimbal2 = new Gimbal2();
